30abb70391d618bc8bac674ddf349aeaea1f61ad
Rubal Tripathi <rubalien@google.com>
2023-06-21 09:43:28 +0000

    Updating code

--- src/main/java/com/heloworld/HelloWorld.java
+++ src/main/java/com/heloworld/HelloWorld.java
@@ -10,23 +10,15 @@ import org.apache.logging.log4j.Logger;
 @RestController
 @SpringBootApplication
 public class HelloWorld {
-    // Create a logger instance for logging messages
     static final Logger logger = LogManager.getLogger(HelloWorld.class);
 
-    // Handler for GET request to the root URL ("/")
-    @GetMapping("/hello")
+    @GetMapping("/hello") 
     public String home() {
-        // Log a message indicating the request
-        logger.info("Request received for /");
-        // Return a "Hello, World!" message
         return "Hello, World!";
     }
 
-    // Application entry point
     public static void main(String[] args) {
-        // Start the Spring Boot application
         SpringApplication.run(HelloWorld.class, args);
-        // Log a message indicating the application has started
         logger.info("Application started");
     }
 }

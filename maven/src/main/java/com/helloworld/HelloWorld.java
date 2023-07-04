d1df5180091334029b59f6ec2ce575168a639047
Rubalien30 <134284199+Rubalien30@users.noreply.github.com>
2023-07-04 15:55:08 +0530

    Update HelloWorld.java

--- maven/src/main/java/com/helloworld/HelloWorld.java
+++ maven/src/main/java/com/helloworld/HelloWorld.java
@@ -34,6 +34,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorld.class, args);
-        logger.info("Application started");
+        logger.info("Welcome to aoss-sample-starter-project!");
     }
 }
